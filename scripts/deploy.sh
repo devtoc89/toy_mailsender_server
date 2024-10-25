7a408289a899912f3ba3bf54ed7b3f1eb697bc18
devtoc89 <devtoc89@gmail.com>
2024-10-25 19:11:57 +0900

    fix: deploy log folder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,11 @@
+PROJECT_LOG_PATH="/home/ec2-user/project/log"
 PROJECT_ROOT="/home/ec2-user/project/forduellee_server"
-DEPLOY_LOG="$PROJECT_ROOT/server_deploy.log"
+DEPLOY_LOG="$PROJECT_LOG_PATH/server_deploy.log"
 
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
+mkdir /home/ec2-user/project/log 2> dev/null
 echo "$TIME_NOW > 파일 복사" >> ../$DEPLOY_LOG
 node main
 
